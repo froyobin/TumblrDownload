9c978595dfbd9de94ded020a1ee3dae4e794f4d8
Jacul <jaculing@gmail.com>
2016-05-20 02:50:40 -0400

    mostly UI stuff, print out the summary

--- TumblrDownload/ViewController.m
+++ TumblrDownload/ViewController.m
@@ -7,19 +7,106 @@
 //
 
 #import "ViewController.h"
+#import <TMTumblrSDK/TMAPIClient.h>
+
+#define kKEY_PATH @"save path"
+#define kKEY_API_KEY @"API key"
+#define kKEY_BLOG_NAME @"blog name"
+
+
+@interface ViewController () <NSTextDelegate>
+
+@property (strong) IBOutlet NSTextField *apiKey;
+@property (strong) IBOutlet NSTextView *logOutput;
+@property (strong) IBOutlet NSTextField *savePath;
+@property (strong) IBOutlet NSTextField *blogName;
+
+@end
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    // Do any additional setup after loading the view.
+    NSString *api = [[NSUserDefaults standardUserDefaults] stringForKey:kKEY_API_KEY];
+    api ? self.apiKey.stringValue = api : nil;
+    NSString *path = [[NSUserDefaults standardUserDefaults] stringForKey:kKEY_PATH];
+    path ? self.savePath.stringValue = path : nil;
+    NSString *blog = [[NSUserDefaults standardUserDefaults] stringForKey:kKEY_BLOG_NAME];
+    blog ? self.blogName.stringValue = blog : nil;
 }
 
-- (void)setRepresentedObject:(id)representedObject {
-    [super setRepresentedObject:representedObject];
-
-    // Update the view, if already loaded.
+- (IBAction)clickSelectSavePath:(id)sender {
+    NSOpenPanel *openPanel = [NSOpenPanel openPanel];
+    
+    [openPanel setCanChooseFiles:NO];
+    [openPanel setCanChooseDirectories:YES];
+    
+    if ([openPanel runModal] == NSModalResponseOK) {
+        NSString *path = openPanel.URL.path;
+        self.savePath.stringValue = path;
+        path ? [[NSUserDefaults standardUserDefaults] setValue:path forKey:kKEY_PATH] : nil;
+    }
 }
+
+- (IBAction)clickDownload:(id)sender {
+    NSString *api = self.apiKey.stringValue;
+    NSString *blog = self.blogName.stringValue;
+    NSAssert(api.length && blog.length, @"warn user later");
+
+    [[NSUserDefaults standardUserDefaults] setValue:api forKey:kKEY_API_KEY];
+    [[NSUserDefaults standardUserDefaults] setValue:blog forKey:kKEY_BLOG_NAME];
+    
+    [TMAPIClient sharedInstance].OAuthConsumerKey = api;
+    NSString *fullBlog = [NSString stringWithFormat:@"%@.tumblr.com", self.blogName.stringValue];
+    
+    [self downloadLikesFromBlog:fullBlog];
+}
+
+- (void)downloadLikesFromBlog:(NSString *)fullBlog
+{
+    [[TMAPIClient sharedInstance] likes:fullBlog
+                             parameters:@{@"limit" : @"30", @"offset" : @"300"}
+                               callback:^(NSDictionary *result, NSError *error) {
+                                   if (error) {
+                                       return;
+                                   }
+                                   [self downloadPostsIn:result];
+                            }];
+}
+
+- (void)downloadPostsIn:(NSDictionary *)response
+{
+    NSNumber *likedCount = [response valueForKey:@"liked_count"];
+    NSArray *posts = [response valueForKey:@"liked_posts"];
+    for (NSDictionary *post in posts) {
+        [self downloadPost:post];
+    }
+}
+
+- (void)downloadPost:(NSDictionary *)post
+{
+    NSMutableString *postSummary = [NSMutableString new];
+    [postSummary appendFormat:@"%@\n", post[@"type"]];
+    [postSummary appendFormat:@"%@\n", post[@"timestamp"]];
+    [postSummary appendFormat:@"%@\n", post[@"summary"]];
+    if ([post[@"type"] isEqualToString:@"video"]) {
+        [postSummary appendFormat:@"%@\n", post[@"video_url"]];
+    } else if ([post[@"type"] isEqualToString:@"photo"]) {
+        NSArray *allPhotos = post[@"photos"];
+        for (NSDictionary *singlePhoto in allPhotos) {
+            [postSummary appendFormat:@"%@\n", [singlePhoto valueForKeyPath:@"original_size.url"]];
+        }
+    }
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSAttributedString* attr = [[NSAttributedString alloc] initWithString:postSummary];
+        
+        [[self.logOutput textStorage] appendAttributedString:attr];
+        [self.logOutput scrollRangeToVisible:NSMakeRange([[_logOutput string] length], 0)];
+    });
+}
+
+#pragma mark delegate
 
 @end
