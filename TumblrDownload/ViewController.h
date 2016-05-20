0e1555a240d6a5a23b73c5eb44da41f122797ff6
Jacul <jaculing@gmail.com>
2016-05-20 00:57:46 -0400

    Initial Commit

---
+++ TumblrDownload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TumblrDownload
+//
+//  Created by zxd on 16/5/20.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ TumblrDownload/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TumblrDownload
+//
+//  Created by zxd on 16/5/20.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
